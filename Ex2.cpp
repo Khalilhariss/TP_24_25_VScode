9c8458f1e078789834018d112806bd9cecb133b4
Khalil Hariss <khalil.hariss1@usj.edu.lb>
2024-10-25 15:33:24 +0300

    Finish Ex3

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 using namespace std;
-
+int takeinteger(int lower ,int upper)
+{
+    bool bornInf;
+    bool bornSup;
+    int n;
+    do{
+       cout <<"Enter an Integer greater than " <<lower<<"and lower than" <<upper<<":";
+       cin>>n;
+       bornInf=n<lower;
+       bornSup=n>upper;
+       if(bornInf)
+       {
+        cout<<"Your number is less or equal to "<<lower<<endl;
+       }
+       if(bornSup)
+       {
+        cout<<"Your number is greater than or equal to"<<upper<<endl;
+       }
+    }while(bornInf || bornSup);
+    return n;
+}
 int main()
 {
    
+    cout<<"Enter the integer for which you want to display the multiplication table:";
     int n;
-    bool bornInf;
-    bool bornSup;
-    do{
-      
-    cout<<"Enter a positive integer less than 70: ";
-    cin>>n;
-    bornInf=n<=0;
-    bornSup=n>=70;
-    if(bornInf)
-    {
-        cout<<"Your number is less than or equal to 0!";
-    }
-    if(bornSup)
-    {
-        cout<<"You number is greater than or equal to 70!";
-    }
-
-    } while(bornInf|| bornSup);
-    for (int i=10; i>=0; --i)
+    n=takeinteger(0,70);
+    int bornsupTable;
+    bornsupTable=takeinteger(8,30);
+    int borninfTable;
+    borninfTable=takeinteger(0,8);    
+    for (int i=bornsupTable; i>=borninfTable; --i)
     cout<<n<<"x"<<i<<"="<<n*i<<endl;
     return 0;
 }
