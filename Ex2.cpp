164d4da3d86e8440d45400ed0504754c45ac287b
Khalil Hariss <khalil.hariss1@usj.edu.lb>
2024-10-25 15:11:22 +0300

    Implement Ex2, Part 4: Error message, bad code

--- Ex2.cpp
+++ Ex2.cpp
@@ -8,6 +8,15 @@ int main()
     do{
     cout<<"Enter a positive integer less than 70: ";
     cin>>n;
+    if(n<=0)
+    {
+        cout<<"Your number is less than or equal to 0!";
+    }
+    if(n>=70)
+    {
+        cout<<"You number is greater than or equal to 70!"
+    }
+
     } while(n<=0 || n>=70);
     for (int i=10; i>=0; --i)
     cout<<n<<"x"<<i<<"="<<n*i<<endl;
