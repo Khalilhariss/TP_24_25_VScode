8a25ca35af3b99ff709f7f505b8fcabab31861fc
Khalil Hariss <khalil.hariss1@usj.edu.lb>
2024-10-25 15:05:31 +0300

    Implement Ex2, Part 3: Add condition on 70

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main()
 {
    
     int n;
     do{
-    cout<<"Enter a positive integer: ";
+    cout<<"Enter a positive integer less than 70: ";
     cin>>n;
-    } while(n<=0);
+    } while(n<=0 || n>=70);
     for (int i=10; i>=0; --i)
     cout<<n<<"x"<<i<<"="<<n*i<<endl;
     return 0;
